b0925e541fed29bbc8bf6847f6b5507826a46fe0
tintin40498 <mjunco636@gmail.com>
2026-02-20 21:36:00 -0300

    Add footer to the footer.php file

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<?php
+// includes/footer.php
+?>
+    </main>
+    <footer style="background: #8B4513; color: white; text-align: center; padding: 30px;">
+        <div class="container">
+            <p>&copy; 2024 JServicios - Todos los derechos reservados</p>
+        </div>
+    </footer>
+</body>
+</html>
