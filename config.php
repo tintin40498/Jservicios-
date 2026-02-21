79391ab9613e6487901d8a2f1281572dfd68f6dc
tintin40498 <mjunco636@gmail.com>
2026-02-20 21:19:56 -0300

    Add configuration settings for the application

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+define('SITE_NAME', 'JServicios');
+define('WHATSAPP_NUMBER', '1159085736');
+define('TELEFONO', '1159085736');
+define('EMAIL', 'jservicios636@gmail.com');
+define('UBICACION', 'Guernica, Av. Atahualpa Yupanqui 2698-2600');
+define('ADMIN_USER', 'admin');
+define('ADMIN_PASS', 'jservicios567ñ$#');
+?>
